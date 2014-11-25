435dda90b1c4980033792648621f9465491e534a
bsorrentino <bartolomeo.sorrentino@gmail.com>
2014-11-26 00:03:57 +0100

    update android implementation

--- src/android/CDVBroadcaster.java
+++ src/android/CDVBroadcaster.java
@@ -4,7 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.webkit.ValueCallback;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -18,11 +21,16 @@ import org.json.JSONObject;
  */
 public class CDVBroadcaster extends CordovaPlugin {
 
+    private static String TAG =  CDVBroadcaster.class.getSimpleName();
+
     public static final String EVENTNAME_ERROR = "event name null or empty.";
 
     java.util.Map<String,BroadcastReceiver> receiverMap =
                     new java.util.HashMap<String,BroadcastReceiver>(10);
 
+    private void fireEvent() {
+
+    }
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if( action.equals("fireNativeEvent")) {
@@ -34,61 +42,76 @@ public class CDVBroadcaster extends CordovaPlugin {
             }
             return true;
         }
-        else if( action.equals("addEventListener")) {
+        else {
+            if (action.equals("addEventListener")) {
 
-            final String eventName = args.getString(0);
-            if( eventName==null || eventName.isEmpty() ) {
-                callbackContext.error(EVENTNAME_ERROR);
-                return false;
+                final String eventName = args.getString(0);
+                if (eventName == null || eventName.isEmpty()) {
+                    callbackContext.error(EVENTNAME_ERROR);
+                    return false;
+                }
+                if (!receiverMap.containsKey(eventName)) {
+
+                    final BroadcastReceiver r = new BroadcastReceiver() {
+
+                        @Override
+                        public void onReceive(Context context, final Intent intent) {
+
+                            final Bundle b = intent.getExtras();
+
+                            // parse the JSON passed as a string.
+                            try {
+
+                                final String userData = b.getString("userdata", "{}");
+
+                                final JSONObject json = new JSONObject(userData); // CHECK IF VALID
+
+                                String method = String.format( "window.broadcaster.fireEvent( '%s', %s );", eventName, userData);
+
+                                CDVBroadcaster.this.webView.evaluateJavascript(method, new ValueCallback<String>() {
+                                    @Override
+                                    public void onReceiveValue(String value) {
+                                        Log.d(TAG, "fireEvent executed!");
+                                    }
+                                });
+
+                            } catch (JSONException e) {
+                                Log.e(TAG, "'userdata' is not a valid json object!");
+                            }
+
+                        }
+                    };
+
+                    LocalBroadcastManager.getInstance(super.webView.getContext())
+                            .registerReceiver(r, new IntentFilter(eventName));
+
+                    receiverMap.put(eventName, r);
+                }
+                callbackContext.success();
+
+                return true;
+            } else if (action.equals("removeEventListener")) {
+
+                final String eventName = args.getString(0);
+                if (eventName == null || eventName.isEmpty()) {
+                    callbackContext.error(EVENTNAME_ERROR);
+                    return false;
+                }
+
+                BroadcastReceiver r = receiverMap.remove(eventName);
+
+                if (r != null) {
+
+                    LocalBroadcastManager.getInstance(super.webView.getContext())
+                            .unregisterReceiver(r);
+
+
+                }
+                callbackContext.success();
+                return true;
             }
-            if( !receiverMap.containsKey(eventName)) {
-
-                final BroadcastReceiver r = new BroadcastReceiver() {
-
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-
-                    }
-                };
-
-                LocalBroadcastManager.getInstance(super.webView.getContext())
-                        .registerReceiver( r, new IntentFilter(eventName));
-
-                receiverMap.put(eventName,r);
-            }
-            callbackContext.success();
-
-            return true;
-        }
-        else if( action.equals("removeEventListener")) {
-
-            final String eventName = args.getString(0);
-            if( eventName==null || eventName.isEmpty() ) {
-                callbackContext.error(EVENTNAME_ERROR);
-                return false;
-            }
-
-            BroadcastReceiver r = receiverMap.remove(eventName);
-
-            if( r!=null  ) {
-
-                LocalBroadcastManager.getInstance(super.webView.getContext())
-                        .unregisterReceiver( r );
-
-
-            }
-            callbackContext.success();
-            return true;
         }
         return false;
-    }
-
-    private void fireEvent(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
     }
 
     @Override
@@ -96,6 +119,14 @@ public class CDVBroadcaster extends CordovaPlugin {
         super.onDestroy();
 
         // deregister receiver
+        for( BroadcastReceiver r : receiverMap.values() ) {
+            LocalBroadcastManager.getInstance(super.webView.getContext())
+                    .unregisterReceiver(r);
+
+        }
+
+        receiverMap.clear();
+
     }
 
 }
