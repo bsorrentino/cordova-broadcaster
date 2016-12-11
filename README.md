a8662bc97609226a5a11346e15e02539cc9c4028
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-12-11 19:47:49 +0100

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# CORDOVA BROADCASTER PLUGIN TEST APPLICATION
+
+## Install plugin
+```
+cordova plugin rm cordova-plugin-broadcaster
+cordova plugin add cordova-plugin-broadcaster
+```
---
+++ android-assets/src/org/bsc/broadcasterApp/MainActivity.java
@@ -0,0 +1,87 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package org.bsc.broadcasterApp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import org.apache.cordova.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
+
+        onJSMessage();
+    }
+
+    void onJSMessage() {
+
+        final BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                try {
+                    final JSONObject data = new JSONObject( intent.getExtras().getString("userdata"));
+
+
+                    Log.d("CDVBroadcaster",
+                            String.format("Native event [%s] received with data [%s]", intent.getAction(), String.valueOf(data)));
+
+                } catch (JSONException e) {
+                   throw new RuntimeException(e);
+                }
+                finally {
+                    sendJSMessage();
+                }
+            }
+        };
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(receiver, new IntentFilter("test.event"));
+
+    }
+    void sendJSMessage() {
+        final Intent intent = new Intent("didShow");
+
+        Bundle b = new Bundle();
+        b.putString( "userdata", "{ data: 'test'}" );
+        intent.putExtras( b);
+
+        LocalBroadcastManager.getInstance(this).sendBroadcastSync(intent);
+    }
+}
