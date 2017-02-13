38668e0d32ef581147bfb2d117ea942c8c566f51
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-02-13 21:58:11 +0100

    fix issue #7

--- www/broadcaster.js
+++ www/broadcaster.js
@@ -42,6 +42,7 @@ module.exports = {
      else {
        this._channels[eventname].subscribe(f);
      }
+     return f;
   },
   removeEventListener: function(eventname, f) {
      if (eventname in this._channels) {
