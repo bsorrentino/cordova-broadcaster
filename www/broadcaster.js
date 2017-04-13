400efd70500bacafed11cdfd0244cf6231f0ce94
bsorrentino <bartolomeo.sorrentino@gmail.com>
2017-04-13 14:53:06 +0200

    fix issue #15

--- www/broadcaster.js
+++ www/broadcaster.js
@@ -2,57 +2,98 @@
 var exec = require('cordova/exec');
 var channel = require('cordova/channel');
 
-module.exports = {
+function Broadcaster() {
+  console.log( "NEW BROADCASTER");
+  this._channels = {};
 
-  _channels: {},
-  createEvent: function(type, data) {
-      var event = document.createEvent('Event');
-      event.initEvent(type, false, false);
-      if (data) {
-          for (var i in data) {
-              if (data.hasOwnProperty(i)) {
-                  event[i] = data[i];
-              }
+  this.channelExists = function( c ) {
+    //return (c in this._channels);
+    return this._channels.hasOwnProperty(c);
+  }
+
+  this.channelCreate = function( c ) {
+    console.log( "CHANNEL " + c + " CREATED! ");
+    this._channels[c] = channel.create(c);
+  }
+  this.channelSubscribe = function( c, f ) {
+    var channel = this._channels[c];
+    channel.subscribe(f);
+    console.log( "CHANNEL " + c + " SUBSCRIBED! " + channel.numHandlers);
+    return channel.numHandlers;
+  }
+  this.channelUnsubscribe = function( c, f ) {
+    var channel = this._channels[c];
+    channel.unsubscribe(f);
+    console.log( "CHANNEL " + c + " UNSUBSCRIBED! " + channel.numHandlers);
+    return channel.numHandlers;
+  }
+  this.channelFire = function( event ) {
+    console.log( "CHANNEL " + event.type + " FIRED! ");
+    this._channels[event.type].fire(event);
+  }
+  this.channelDelete = function( c ) {
+    delete this._channels[c];
+    console.log( "CHANNEL " + c + " DELETED! ");
+
+  }
+
+}
+
+Broadcaster.prototype.fireNativeEvent = function(eventname, data, success, error) {
+     exec(success, error, "broadcaster", "fireNativeEvent", [ eventname, data ]);
+}
+
+Broadcaster.prototype.fireEvent = function(type, data) {
+  if( !this.channelExists(type) ) return;
+
+  var event = document.createEvent('Event');
+  event.initEvent(type, false, false);
+  if (data) {
+      for (var i in data) {
+          if (data.hasOwnProperty(i)) {
+              event[i] = data[i];
           }
       }
-      return event;
-  },
-  fireNativeEvent: function (eventname, data, success, error) {
-     exec(success, error, "broadcaster", "fireNativeEvent", [ eventname, data ]);
-  },
-  fireEvent: function (type, data) {
-     var event = this.createEvent( type, data );
-     if (event && (event.type in this._channels)) {
-         this._channels[event.type].fire(event);
-     }
-  },
-  //removeChannel: function( eventname ) {
-  //  delete this._channels[eventname];
-  //},
-  addEventListener: function (eventname,f) {
-     if (!(eventname in this._channels)) {
-         var me = this;
-         exec( function() {
-           me._channels[eventname] = channel.create(eventname);
-           me._channels[eventname].subscribe(f);
-         }, function(err)  {
-           console.log( "ERROR addEventListener: ", err)
-         }, "broadcaster", "addEventListener", [ eventname ]);
-     }
-     else {
-       this._channels[eventname].subscribe(f);
-     }
-     return f;
-  },
-  removeEventListener: function(eventname, f) {
-     if (eventname in this._channels) {
+  }
+  this.channelFire( event );
+}
+
+function _debug( msg, o ) {
+  console.log( msg );
+  for( var m in o ) {
+    console.log( "==> " + m);
+  }
+}
+
+Broadcaster.prototype.addEventListener = function (eventname,f) {
+
+   if (!this.channelExists(eventname)) {
+       this.channelCreate(eventname);
+       var me = this;
+       exec( function() {
+         me.channelSubscribe(eventname,f);
+       }, function(err)  {
+         console.log( "ERROR addEventListener: ", err)
+       }, "broadcaster", "addEventListener", [ eventname ]);
+   }
+   else {
+     this.channelSubscribe(eventname,f);
+   }
+}
+
+Broadcaster.prototype.removeEventListener = function(eventname, f) {
+
+   if (this.channelExists(eventname)) {
+      if( this.channelUnsubscribe(eventname, f) === 0 ) {
         var me = this;
         exec( function() {
-          me._channels[eventname].unsubscribe(f);
+          me.channelDelete(eventname);
         }, function(err)  {
           console.log( "ERROR removeEventListener: ", err)
         }, "broadcaster", "removeEventListener", [ eventname ]);
-     }
-  }
 
-};
+      }
+   }
+}
+
+module.exports = new Broadcaster();
