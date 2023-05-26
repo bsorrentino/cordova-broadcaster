0d3fd3d96e9439819ab81fdb6fbaff1ad98db8b9
phyr0s <11390548+phyr0s@users.noreply.github.com>
2023-05-26 12:44:16 +0200

    Update CDVBroadcaster.java

--- src/android/CDVBroadcaster.java
+++ src/android/CDVBroadcaster.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import androidx.core.content.LocalBroadcastManager;
+import androidx.content.LocalBroadcastManager;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
